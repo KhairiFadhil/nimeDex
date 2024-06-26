5e3505c54d3f4248ef53fda861c96df97d6311e2
KhairiFadhil <raidfadhil.faiq@gmail.com>
2024-06-26 14:07:54 +0700

    small changes

--- src/app/(chapter)/chapter/[keyword]/page.jsx
+++ src/app/(chapter)/chapter/[keyword]/page.jsx
@@ -1,4 +1,8 @@
-import { FetchChapterImage, FetchMangaChapter, FetchMangaChapterFeed } from "@/lib/api/anime-api";
+import {
+  FetchChapterImage,
+  FetchMangaChapter,
+  FetchMangaChapterFeed,
+} from "@/lib/api/anime-api";
 import MangaPage from "../../component/mangapage";
 import SideMenu from "../../component/sidemenu";
 import List from "../../component/list";
@@ -7,39 +11,61 @@ async function Page({ params }) {
   const id = params.keyword;
   const response = await FetchChapterImage(id);
 
-    if (!response) {
-      throw new Error(error || 'An unexpected error occured')
-    }
-
-  const mangaResponse = await FetchMangaChapter(id)
-  const responseManga = mangaResponse?.data
-
-  if(!responseManga) {
-    throw new Error(error || 'Failed to fetch chapter')
+  if (!response) {
+    throw new Error(error || "An unexpected error occured");
   }
 
-  const MangaData = responseManga?.relationships?.find(relationship => relationship.type === 'manga');
-  const responseChapterFeed = await FetchMangaChapterFeed(MangaData.id, responseManga.attributes.translatedLanguage);
+  const mangaResponse = await FetchMangaChapter(id);
+  const responseManga = mangaResponse?.data;
+
+  if (!responseManga) {
+    throw new Error(error || "Failed to fetch chapter");
+  }
+
+  const MangaData = responseManga?.relationships?.find(
+    (relationship) => relationship.type === "manga"
+  );
+  const responseChapterFeed = await FetchMangaChapterFeed(
+    MangaData.id,
+    responseManga.attributes.translatedLanguage
+  );
   const chapterFeed = responseChapterFeed?.data;
-  
+
   //mencari chapter selanjutnya \
-  const prevFilterChapter =  chapterFeed.filter(chapter => chapter?.attributes.chapter == Number(responseManga?.attributes?.chapter) - 1)
-  const nextFilterChapter = chapterFeed.filter(chapter => chapter?.attributes.chapter == Number(responseManga?.attributes?.chapter) + 1)
+  const prevFilterChapter = chapterFeed.filter(
+    (chapter) =>
+      chapter?.attributes.chapter ==
+      Number(responseManga?.attributes?.chapter) - 1
+  );
+  const nextFilterChapter = chapterFeed.filter(
+    (chapter) =>
+      chapter?.attributes.chapter ==
+      Number(responseManga?.attributes?.chapter) + 1
+  );
   //menentukan chapter dengan group yang sama jika tidak akan ke chapter yang ada
-  const prevChapterGroup = prevFilterChapter.find(chapter => chapter.relationships[0].id == responseManga.relationships[0].id);
-  let nextChapter, prevChapter
+  const prevChapterGroup = prevFilterChapter.find(
+    (chapter) =>
+      chapter.relationships[0].id == responseManga.relationships[0].id
+  );
+  let nextChapter, prevChapter;
   if (prevFilterChapter.length === 0) {
-    prevChapter = false
+    prevChapter = false;
   } else {
-    const prevChapterGroup = prevFilterChapter.find(chapter => chapter.relationships[0].id == responseManga.relationships[0].id);
-    prevChapter =  prevChapterGroup.id || prevFilterChapter[0].id
+    const prevChapterGroup = prevFilterChapter.find(
+      (chapter) =>
+        chapter.relationships[0].id == responseManga.relationships[0].id
+    );
+    prevChapter = prevChapterGroup.id || prevFilterChapter[0].id;
   }
-  console.log(prevChapter)
+  console.log(prevChapter);
   if (nextFilterChapter.length === 0) {
     nextChapter = false;
   } else {
-    const nextChapterGroup = nextFilterChapter.find(chapter => chapter.relationships[0].id == responseManga.relationships[0].id);
-    nextChapter =  nextChapterGroup.id || nextFilterChapter[0].id
+    const nextChapterGroup = nextFilterChapter.find(
+      (chapter) =>
+        chapter.relationships[0].id == responseManga.relationships[0].id
+    );
+    nextChapter = nextChapterGroup.id || nextFilterChapter[0].id;
   }
   // if (chapterFeed){
   //   const index = chapterFeed.findIndex(
@@ -60,24 +86,27 @@ async function Page({ params }) {
 
   return (
     <div className="w-full h-full relative block">
-      <List 
-      chapterNum={responseManga?.attributes?.chapter} 
-      mangaInfo={MangaData} 
-      chapterFeed={chapterFeed}/>
+      <List
+        chapterNum={responseManga?.attributes?.chapter}
+        mangaInfo={MangaData}
+        chapterFeed={chapterFeed}
+      />
       <SideMenu
-      chapterNum={responseManga?.attributes?.chapter} 
-      mangaInfo={MangaData} 
-      nextChapter={nextChapter}
-      prevChapter={prevChapter}
-       />
+        chapterNum={responseManga?.attributes?.chapter}
+        mangaInfo={MangaData}
+        nextChapter={nextChapter}
+        prevChapter={prevChapter}
+      />
       <div className="w-full h-full relative block z-10">
         <div className="w-full h-full max-w-[1440px] mx-auto bg-slate-200">
-          <MangaPage api={response}
-          chapterFeed={chapterFeed} />
+          <MangaPage api={response} chapterFeed={chapterFeed} />
         </div>
       </div>
       <div>
-        <button hre className="w-full h-[70px] bg-red-700 hover:bg-red-600 active:bg-red-400 transition-all">
+        <button
+          hre
+          className="w-full h-[70px] bg-red-700 hover:bg-red-600 active:bg-red-400 transition-all"
+        >
           Next Chapter
         </button>
       </div>
